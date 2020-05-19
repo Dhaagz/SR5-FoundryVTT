77a738c2a819f0bfc72e81b49547323e35683dec
Hermanno <hermanno@free.fr>
2020-05-19 23:36:25 +0200

    Fixed skill filter when actor has no specs

--- module/actor/sheet.js
+++ module/actor/sheet.js
@@ -108,7 +108,7 @@ export class SR5ActorSheet extends ActorSheet {
 
   _doesSkillContainText(key, skill, text) {
     if(!skill.specs){
-      skill.specs = "";
+      skill.specs = [];
     }
     let searchString = `${key} ${game.i18n.localize(skill.label)} ${skill.specs.join(' ')}`;
     return searchString.toLowerCase().search(text.toLowerCase()) > -1;
